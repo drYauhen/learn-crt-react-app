f18a5524aa4ec41ae86472ace03aac0298bf23db
Eugene Goncharov <eugene.goncharov@dfo.global>
2016-12-01 17:18:07 -0800

    up to video 6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 import React from "react";
 
-const App = () => <h1>Hi</h1>
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            txt: "this is the new state"
+        }
+    }
+    update(e) {
+        console.log(e.target);
+        console.log(this);
+        this.setState({txt: e.target.value});
+    }
+    render(){
+        let th = this;
+        return (
+            <div>
+            <input type="text" onChange={th.update.bind(th)} />
+                <h1>Hello World</h1>
+                <b>{this.props.txt}</b>
+                <h2>{this.state.txt}</h2>
+             </div>
+        )
+    }
+}
+
+App.propTypes = {
+    txt: React.PropTypes.string.isRequired,
+    cat: React.PropTypes.number.isRequired
+}
 
 export default App;
